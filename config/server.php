22ac825f1c201ddafb2a1e961014b308ccbe8180
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-08-15 00:49:06 +0800

    Update server.php

--- config/server.php
+++ config/server.php
@@ -13,19 +13,19 @@
  */
 
 return [
-    'listen' => 'http://0.0.0.0:8787',
-    'transport' => 'tcp',
-    'context' => [],
-    'name' => 'webman',
-    'count' => cpu_count() * 4,
-    'user' => '',
-    'group' => '',
-    'reusePort' => false,
-    'event_loop' => '',
-    'stop_timeout' => 2,
-    'pid_file' => runtime_path() . '/webman.pid',
-    'status_file' => runtime_path() . '/webman.status',
-    'stdout_file' => runtime_path() . '/logs/stdout.log',
-    'log_file' => runtime_path() . '/logs/workerman.log',
+    'listen'           => 'http://0.0.0.0:' . env('APP_PORT', 8555),
+    'transport'        => 'tcp',
+    'context'          => [],
+    'name'             => 'webman',
+    'count'            => cpu_count() * 4,
+    'user'             => '',
+    'group'            => '',
+    'reusePort'        => false,
+    'event_loop'       => '',
+    'stop_timeout'     => 2,
+    'pid_file'         => runtime_path() . '/webman.pid',
+    'status_file'      => runtime_path() . '/webman.status',
+    'stdout_file'      => runtime_path() . '/logs/stdout.log',
+    'log_file'         => runtime_path() . '/logs/workerman.log',
     'max_package_size' => 10 * 1024 * 1024
 ];
