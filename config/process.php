ab64be1793217de02641ce9ededa861f8c840654
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-08-16 18:05:10 +0800

    Update process.php

--- config/process.php
+++ config/process.php
@@ -28,6 +28,7 @@ return [
                 base_path() . '/support',
                 base_path() . '/resource',
                 base_path() . '/.env',
+                base_path() . '/common',
             ], glob(base_path() . '/plugin/*/app'), glob(base_path() . '/plugin/*/config'), glob(base_path() . '/plugin/*/api')),
             // Files with these suffixes will be monitored
             'monitorExtensions' => [
