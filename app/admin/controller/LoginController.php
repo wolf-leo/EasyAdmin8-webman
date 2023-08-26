2c849d446579ccfb2d9bfaef3f9029c9d058123a
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-08-27 02:05:32 +0800

    Update LoginController.php

--- app/admin/controller/LoginController.php
+++ app/admin/controller/LoginController.php
@@ -7,27 +7,28 @@ use common\controller\AdminController;
 use support\Request;
 use support\Response;
 use Respect\Validation\Validator;
+use Webman\Captcha\CaptchaBuilder;
+use Webman\Captcha\PhraseBuilder;
 
 class LoginController extends AdminController
 {
 
     public function index(Request $request): Response
     {
+        $captcha = env('EASYADMIN.CAPTCHA', false);
         if (!$request->isAjax()) {
-            $captcha = env('EASYADMIN.CAPTCHA', 1);
             return $this->fetch('', compact('captcha'));
         }
-
-        $post  = $request->post();
-        $rules = [
-            'username'   => 'required',
-            'password'   => 'required',
-            'keep_login' => 'required',
-        ];
+        $post = $request->post();
         Validator::input($post, [
             'username' => Validator::notEmpty()->setName('用户名'),
             'password' => Validator::notEmpty()->setName('密码')
         ]);
+        if ($captcha) {
+            if (strtolower($request->post('captcha')) !== $request->session()->get('captcha')) {
+                return $this->error('图片验证码错误');
+            }
+        }
         $admin = SystemAdmin::where(['username' => $post['username']])->first();
         if (empty($admin) || password($post['password']) != $admin->password) {
             return $this->error('用户名或密码有误');
@@ -43,6 +44,25 @@ class LoginController extends AdminController
         $admin['expire_time'] = $post['keep_login'] == 1 ? true : time() + 7200;
         session(compact('admin'));
         return $this->success('登录成功', [], __url());
+    }
+
+    /**
+     * 输出验证码图像
+     */
+    public function captcha(Request $request): Response
+    {
+        $length  = 4;
+        $chars   = '0123456789';
+        $phrase  = new PhraseBuilder($length, $chars);
+        $builder = new CaptchaBuilder(null, $phrase);
+        // 生成验证码
+        $builder->build();
+        // 将验证码的值存储到session中
+        $request->session()->set('captcha', strtolower($builder->getPhrase()));
+        // 获得验证码图片二进制数据
+        $img_content = $builder->get();
+        // 输出验证码二进制数据
+        return response($img_content, 200, ['Content-Type' => 'image/jpeg']);
     }
 
     public function out(Request $request): Response
