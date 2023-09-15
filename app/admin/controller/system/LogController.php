20a98f7bd31c2a64f4e0deb0c69661683ab9bebc
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-09-15 23:31:23 +0800

    新增log日志导出

--- app/admin/controller/system/LogController.php
+++ app/admin/controller/system/LogController.php
@@ -6,6 +6,10 @@ use app\admin\model\SystemLog;
 use common\controller\AdminController;
 use common\services\annotation\ControllerAnnotation;
 use common\services\annotation\NodeAnnotation;
+use common\services\tool\CommonTool;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use support\Db;
 use support\Request;
 use support\Response;
 
@@ -20,6 +24,9 @@ class LogController extends AdminController
         $this->model = new SystemLog();
     }
 
+    /**
+     * @NodeAnnotation(title="列表")
+     */
     public function index(Request $request): Response
     {
         if (!$request->isAjax()) return $this->fetch();
@@ -29,7 +36,7 @@ class LogController extends AdminController
         try {
             $count = $this->model->setMonth($month)->where($where)->count();
             $list  = $this->model->setMonth($month)->where($where)->orderByDesc($this->order)->with(['admin'])->paginate($limit)->items();
-        } catch (\PDOException | \Exception $exception) {
+        } catch (\PDOException|\Exception $exception) {
             $count = 0;
             $list  = [];
         }
@@ -41,4 +48,61 @@ class LogController extends AdminController
         ];
         return json($data);
     }
+
+    /**
+     * @NodeAnnotation(title="导出")
+     */
+    public function export(Request $request): Response|bool
+    {
+        # 功能简单，请根据业务自行扩展
+        [$page, $limit, $where, $excludeFields] = $this->buildTableParams(['month']);
+        $tableName = $this->model->getTable();
+        $tableName = CommonTool::humpToLine(lcfirst($tableName));
+        $prefix    = config('database.connections.mysql.prefix');
+        $dbList    = Db::select("show full columns from {$prefix}{$tableName}");
+        $header    = [];
+        foreach ($dbList as $vo) {
+            $comment = !empty($vo->Comment) ? $vo->Comment : $vo->Field;
+            if (!in_array($vo->Field, $this->noExportFields)) {
+                $header[] = [$comment, $vo->Field];
+            }
+        }
+        $month = !empty($excludeFields['month']) ? date('Ym', strtotime($excludeFields['month'])) : date('Ym');
+        if (empty($month)) $month = date('Ym');
+        try {
+            $list = $this->model->setMonth($month)->where($where)->orderByDesc('id')->limit(100000)->get();
+        } catch (\PDOException|\Exception $exception) {
+            return $this->error($exception->getMessage());
+        }
+        if (empty($list)) return $this->error('暂无数据');
+        $list     = $list->toArray();
+        $fileName = '后台导出文件';
+        try {
+            $excelKeys = [];
+            for ($x = 'A'; $x != 'IW'; $x++) $excelKeys[] = $x;
+            $spreadsheet = new Spreadsheet();
+            $sheet       = $spreadsheet->getActiveSheet();
+            $countHeader = count($header);
+            $headerKeys  = [];
+            for ($i = 0; $i < $countHeader; $i++) {
+                $sheet->setCellValue($excelKeys[$i] . '1', $header[$i][0] ?? '');
+                $headerKeys [] = $header[$i][1] ?? '';
+            }
+            foreach ($list as $key => $value) {
+                for ($j = 0; $j < $countHeader; $j++) {
+                    $_val = $value[$headerKeys[$j]] ?? '';
+                    $sheet->setCellValue($excelKeys[$j] . ($key + 2), $_val . "\t");
+                }
+            }
+            $writer    = new Xlsx($spreadsheet);
+            $file_path = runtime_path() . '/' . $fileName . '.xlsx';
+            // 保存文件到 public 下
+            $writer->save($file_path);
+            // 下载文件
+            return response()->download($file_path, $fileName . '.xlsx');
+        } catch (\Exception|\PhpOffice\PhpSpreadsheet\Exception$e) {
+            return $this->error($e->getMessage());
+        }
+    }
+
 }
