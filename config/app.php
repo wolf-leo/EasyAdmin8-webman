205b4716fe777166047a64658df4a9c5fcaefdb1
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-11-21 00:23:33 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -15,12 +15,12 @@
 use support\Request;
 
 return [
-    'debug' => true,
-    'error_reporting' => E_ALL,
-    'default_timezone' => 'Asia/Shanghai',
-    'request_class' => Request::class,
-    'public_path' => base_path() . DIRECTORY_SEPARATOR . 'public',
-    'runtime_path' => base_path(false) . DIRECTORY_SEPARATOR . 'runtime',
+    'debug'             => env('APP_DEBUG', true),
+    'error_reporting'   => E_ALL,
+    'default_timezone'  => 'Asia/Shanghai',
+    'request_class'     => Request::class,
+    'public_path'       => base_path() . DIRECTORY_SEPARATOR . 'public',
+    'runtime_path'      => base_path(false) . DIRECTORY_SEPARATOR . 'runtime',
     'controller_suffix' => 'Controller',
-    'controller_reuse' => false,
+    'controller_reuse'  => false,
 ];
