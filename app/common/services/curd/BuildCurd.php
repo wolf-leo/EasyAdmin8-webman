366d22364e885600231a38fc75379ea5d28d08a4
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2024-03-24 18:57:32 +0800

    Update BuildCurd.php

--- app/common/services/curd/BuildCurd.php
+++ app/common/services/curd/BuildCurd.php
@@ -276,11 +276,8 @@ class BuildCurd
                 }
 
             }
-
-            // 获取表名注释
-            $tableSchema        = Db::select("SELECT table_name,table_comment FROM information_schema.TABLES WHERE table_schema = 'easyadmin' AND table_name = '{$this->tablePrefix}{$this->table}'");
-            $this->tableComment = (isset($tableSchema[0]['table_comment']) && !empty($tableSchema[0]['table_comment'])) ? $tableSchema[0]['table_comment'] : $this->table;
-        }catch (\Exception $e) {
+            $this->tableComment = $this->table;
+        } catch (\Exception $e) {
             throw new TableException($e->getMessage());
         }
 
@@ -288,11 +285,11 @@ class BuildCurd
         $nodeArray = explode('_', $this->table);
         if (count($nodeArray) == 1) {
             $this->controllerFilename = ucfirst($nodeArray[0]);
-        }else {
+        } else {
             foreach ($nodeArray as $k => $v) {
                 if ($k == 0) {
                     $this->controllerFilename = "{$v}{$this->DS}";
-                }else {
+                } else {
                     $this->controllerFilename .= ucfirst($v);
                 }
             }
@@ -376,7 +373,7 @@ class BuildCurd
             }
             $this->relationArray[$relationTable] = $relation;
             $this->selectFileds[]                = $foreignKey;
-        }catch (\Exception $e) {
+        } catch (\Exception $e) {
             throw new TableException($e->getMessage());
         }
         return $this;
@@ -666,7 +663,7 @@ class BuildCurd
                         }
                     }
                     !empty($formatDefine) && $column['define'] = $formatDefine;
-                }else {
+                } else {
                     $column['define'] = $define;
                 }
             }
@@ -979,7 +976,7 @@ class BuildCurd
         $controllerFile = "{$this->rootDir}{$this->DS}app{$this->DS}admin{$this->DS}controller{$this->DS}{$this->controllerFilename}Controller.php";
         if (empty($this->relationArray)) {
             $controllerIndexMethod = '';
-        }else {
+        } else {
             $relationCode = '';
             foreach ($this->relationArray as $key => $val) {
                 $relation     = CommonTool::lineToHump($key);
@@ -1030,7 +1027,7 @@ class BuildCurd
         $modelFile = "{$this->rootDir}{$this->DS}app{$this->DS}admin{$this->DS}model{$this->DS}{$this->modelFilename}.php";
         if (empty($this->relationArray)) {
             $relationList = '';
-        }else {
+        } else {
             $relationList = '';
             foreach ($this->relationArray as $key => $val) {
                 $relation     = CommonTool::lineToHump($key);
@@ -1221,33 +1218,33 @@ class BuildCurd
 
             if ($val['formType'] == 'image') {
                 $templateValue = "{field: '{$field}', title: '{$val['comment']}', templet: ea.table.image}";
-            }elseif ($val['formType'] == 'images') {
+            } elseif ($val['formType'] == 'images') {
                 continue;
-            }elseif ($val['formType'] == 'file') {
+            } elseif ($val['formType'] == 'file') {
                 $templateValue = "{field: '{$field}', title: '{$val['comment']}', templet: ea.table.url}";
-            }elseif ($val['formType'] == 'files') {
+            } elseif ($val['formType'] == 'files') {
                 continue;
-            }elseif ($val['formType'] == 'editor') {
+            } elseif ($val['formType'] == 'editor') {
                 continue;
-            }elseif (in_array($field, $this->switchFields)) {
+            } elseif (in_array($field, $this->switchFields)) {
                 if (isset($val['define']) && !empty($val['define'])) {
                     $values        = json_encode($val['define'], JSON_UNESCAPED_UNICODE);
                     $templateValue = "{field: '{$field}', search: 'select', selectList: {$values}, title: '{$val['comment']}', templet: ea.table.switch}";
-                }else {
+                } else {
                     $templateValue = "{field: '{$field}', title: '{$val['comment']}', templet: ea.table.switch}";
                 }
-            }elseif (in_array($val['formType'], ['select', 'checkbox', 'radio', 'switch'])) {
+            } elseif (in_array($val['formType'], ['select', 'checkbox', 'radio', 'switch'])) {
                 if (isset($val['define']) && !empty($val['define'])) {
                     $values        = json_encode($val['define'], JSON_UNESCAPED_UNICODE);
                     $templateValue = "{field: '{$field}', search: 'select', selectList: {$values}, title: '{$val['comment']}'}";
-                }else {
+                } else {
                     $templateValue = "{field: '{$field}', title: '{$val['comment']}'}";
                 }
-            }elseif (in_array($field, ['remark'])) {
+            } elseif (in_array($field, ['remark'])) {
                 $templateValue = "{field: '{$field}', title: '{$val['comment']}', templet: ea.table.text}";
-            }elseif (in_array($field, $this->sortFields)) {
+            } elseif (in_array($field, $this->sortFields)) {
                 $templateValue = "{field: '{$field}', title: '{$val['comment']}', edit: 'text'}";
-            }else {
+            } else {
                 $templateValue = "{field: '{$field}', title: '{$val['comment']}'}";
             }
 
@@ -1260,23 +1257,23 @@ class BuildCurd
             foreach ($tableVal['tableColumns'] as $field => $val) {
                 if ($val['formType'] == 'image') {
                     $templateValue = "{field: '{$table}.{$field}', title: '{$val['comment']}', templet: ea.table.image}";
-                }elseif ($val['formType'] == 'images') {
+                } elseif ($val['formType'] == 'images') {
                     continue;
-                }elseif ($val['formType'] == 'file') {
+                } elseif ($val['formType'] == 'file') {
                     $templateValue = "{field: '{$table}.{$field}', title: '{$val['comment']}', templet: ea.table.url}";
-                }elseif ($val['formType'] == 'files') {
+                } elseif ($val['formType'] == 'files') {
                     continue;
-                }elseif ($val['formType'] == 'editor') {
+                } elseif ($val['formType'] == 'editor') {
                     continue;
-                }elseif ($val['formType'] == 'select') {
+                } elseif ($val['formType'] == 'select') {
                     $templateValue = "{field: '{$table}.{$field}', title: '{$val['comment']}'}";
-                }elseif (in_array($field, ['remark'])) {
+                } elseif (in_array($field, ['remark'])) {
                     $templateValue = "{field: '{$table}.{$field}', title: '{$val['comment']}', templet: ea.table.text}";
-                }elseif (in_array($field, $this->switchFields)) {
+                } elseif (in_array($field, $this->switchFields)) {
                     $templateValue = "{field: '{$table}.{$field}', title: '{$val['comment']}', templet: ea.table.switch}";
-                }elseif (in_array($field, $this->sortFields)) {
+                } elseif (in_array($field, $this->sortFields)) {
                     $templateValue = "{field: '{$table}.{$field}', title: '{$val['comment']}', edit: 'text'}";
-                }else {
+                } else {
                     $templateValue = "{field: '{$table}.{$field}', title: '{$val['comment']}'}";
                 }
 
